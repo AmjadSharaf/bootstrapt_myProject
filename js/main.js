0bf981fbcd89955d40f636db0eb2ae1bc8069d95
Amjad-Sharaf <amjd96196@gmail.com>
2023-10-28 14:26:49 +0300

    Add main java script file

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function myFunction(x){
+    x.style.background = "yellow";
+}
+function mwfunction(){
+    document.getElementById("help").innerHTML="for help connect on 0964807067  <hr> sent a message on (amjd96196@gmail.com) "
+}
